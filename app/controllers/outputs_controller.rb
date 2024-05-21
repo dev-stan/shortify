2e12a8cfc22dbff4d63d15f7979f1d6ea6832a43
Matthew Davies <m.davies.work@gmail.com>
2024-05-21 11:57:42 +0900

    added url checker, implemented logic and applied to show page also

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -1,7 +1,19 @@
 class OutputsController < ApplicationController
-  def create
-  end
 
   def new
+    @output = Output.new
+  end
+
+  def create
+    @output = Output.new
+    @owner = current_user
+    @source = Source.find(params[:id])
+    if @output.save
+      redirect_to source_path(@source)
+      puts 'saved'
+    else
+      render 'pages/home', status: :unprocessable_entity
+      puts 'not saved'
+    end
   end
 end
