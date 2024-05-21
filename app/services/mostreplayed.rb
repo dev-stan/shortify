5d64a7e72db136fd91673628dfbe2de8f4c2fe10
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-21 06:28:36 +0200

    add methods to replayed class

--- app/services/mostreplayed.rb
+++ app/services/mostreplayed.rb
@@ -1,5 +1,44 @@
 class Mostreplayed
   def initialize(url)
     @url = url
+    @sorted_scores = []
+  end
+
+  def get_response
+    begin
+      response_serialized = URI.open(@url).read
+      response = JSON.parse(response_serialized)
+
+      intensity_scores = response["items"][0]["mostReplayed"]["markers"].map do |marker|
+        {
+          startMillis: marker["startMillis"],
+          intensityScoreNormalized: marker["intensityScoreNormalized"]
+        }
+      end
+
+      @sorted_scores = intensity_scores.sort_by { |score| -score[:intensityScoreNormalized] }
+    rescue OpenURI::HTTPError => e
+      puts "HTTP Error: #{e.message}"
+    rescue JSON::ParserError => e
+      puts "JSON Parsing Error: #{e.message}"
+    rescue StandardError => e
+      puts "An error occurred: #{e.message}"
+    end
+  end
+
+  def write
+    begin
+      File.open("output.json", "w") do |file|
+        file.write(JSON.pretty_generate(@sorted_scores))
+      end
+
+      @sorted_scores.each do |score|
+        puts "Timestamp: #{score[:startMillis]} ms - Intensity Score: #{score[:intensityScoreNormalized]}"
+      end
+    rescue IOError => e
+      puts "File Writing Error: #{e.message}"
+    rescue StandardError => e
+      puts "An error occurred: #{e.message}"
+    end
   end
 end
