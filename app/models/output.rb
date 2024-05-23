846e98f5fbb9ad9ad4434cf1e677568923ad7995
Matthew Davies <m.davies.work@gmail.com>
2024-05-23 15:25:15 +0900

    form now flies through, though needs API

--- app/models/output.rb
+++ app/models/output.rb
@@ -7,6 +7,9 @@ class Output < ApplicationRecord
   FONTSTYLES = ["Normal", "Italic", "Bold"]
   validates :font_style, presence: true, inclusion: { in: FONTSTYLES }
   validates :script, presence: true
+  VOICES = [1, 2, 3]
+  validates :voice, presence: true, inclusion: { in: VOICES }
+
 
   VIDEOS = ["vid1", "vid2", "vid3", "vid4"]
 end
