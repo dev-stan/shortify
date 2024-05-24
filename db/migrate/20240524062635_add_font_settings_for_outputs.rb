9c05bdf0c9b8dc5d4a4189dfb40fa876761a41ac
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-24 10:15:33 +0200

    fix migrations

--- db/migrate/20240524062635_add_font_settings_for_outputs.rb
+++
@@ -1,7 +0,0 @@
-class AddFontSettingsForOutputs < ActiveRecord::Migration[7.1]
-  def change
-    add_column :sources, :font_size, :integer
-    add_column :sources, :font_style, :string
-    add_column :sources, :font_family, :string
-  end
-end
