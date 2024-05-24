472da37d5ebe0c3269208a5f87a4f0ce45c30b75
kimberly tecson <tecsonkim@gmail.com>
2024-05-24 13:23:35 +0900

    checked if api is working with the controller

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -12,6 +12,8 @@ end
 
   def create
     @output = Output.new(output_params)
+    @output.url = GenerateVideo.new('https://drive.google.com/uc?export=download&id=181aL20cO8Jvvpz4MtWvvyEoysQjyZUdh', @output.script).final_video_link
+    @output.source = 'https://drive.google.com/uc?export=download&id=181aL20cO8Jvvpz4MtWvvyEoysQjyZUdh'
     @output.user = current_user
         if @output.save
       redirect_to sources_path
@@ -20,11 +22,13 @@ end
     else
       render 'pages/home', status: :unprocessable_entity
       puts 'not saved'
+      p @output.valid?
+      p @output.errors.messages
       flash.alert = "Output failed."
     end
   end
 
   def output_params
-    params.require(:output).permit(:source_id, :font_family, :font_style, :font_size, :script, :voice)
+    params.require(:output).permit(:script, :font_family, :font_style, :voice)
   end
 end
