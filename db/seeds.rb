d6d55c3c635ffddd66efbb0d3af4f3e2274aded5
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-24 10:21:20 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,14 @@ puts 'Deleted all sources'
 
 videos = ['source-1', 'source-2', 'source-3']
 
-# # Create new Source instances and attach videos
-# videos.each_with_index do |video, index|
-#   source = Source.new(url: 'hello', location: 'world')
-#   source.video.attach(io: File.open("app/assets/images/#{video}.mp4"), filename: "#{video}.mp4", content_type: 'video/mp4')
-#   source.url = source.video.url
-#   source.save!
-#   puts "Seeded Source ##{index + 1} with video #{video}, url: #{source.url}"
-# end
+# Create new Source instances and attach videos
+videos.each_with_index do |video, index|
+  source = Source.new(url: 'hello', location: 'world')
+  # source.video.attach(io: File.open("app/assets/images/#{video}.mp4"), filename: "#{video}.mp4", content_type: 'video/mp4')
+  source.video.attach(io: 'https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529190/lazyreel-seeds/story-video-1.mp4', filename: "#{video}.mp4", content_type: 'video/mp4')
+  source.url = source.video.url
+  source.save!
+  puts "Seeded Source ##{index + 1} with video #{video}, url: #{source.url}"
+end
 
-# puts "Created #{videos.size} and attached videos"
-
-
-
-video.attach(io: 'https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529190/lazyreel-seeds/story-video-1.mp4', filename: "#{video}.mp4", content_type: 'video/mp4')
+puts "Created #{videos.size} and attached videos"
