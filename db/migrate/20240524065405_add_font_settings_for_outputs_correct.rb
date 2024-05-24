49b69de5b558c9ee05bd1a22ca3078d0751fc2c5
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-24 08:55:03 +0200

    fix migration

---
+++ db/migrate/20240524065405_add_font_settings_for_outputs_correct.rb
@@ -0,0 +1,7 @@
+class AddFontSettingsForOutputsCorrect < ActiveRecord::Migration[7.1]
+  def change
+    add_column :outputs, :font_size, :integer
+    add_column :outputs, :font_style, :string
+    add_column :outputs, :font_family, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_062635) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_065405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_062635) do
     t.datetime "updated_at", null: false
     t.text "script"
     t.integer "voice"
+    t.integer "font_size"
+    t.string "font_style"
+    t.string "font_family"
     t.index ["source_id"], name: "index_outputs_on_source_id"
     t.index ["user_id"], name: "index_outputs_on_user_id"
   end
