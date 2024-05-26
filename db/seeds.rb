b162fdb30eab637906392c24abb9773b9b03e5b5
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-26 09:40:49 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,22 @@ Output.destroy_all
 Source.destroy_all
 puts 'Deleted all sources'
 
-videos = ['source-1']
+videos = [
+  'https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529220/lazyreel-seeds/source-1.mp4',
+  'https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529211/lazyreel-seeds/source-2.mp4',
+  'https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529209/lazyreel-seeds/source-3.mp4',
+
+]
 
 # Create new Source instances and attach videos
 videos.each_with_index do |video, index|
-  source = Source.new(url: 'hello', location: 'world')
+  source = Source.new(url: 'url not needed - stan', location: 'location not needed - stan')
   # source.video.attach(io: File.open("app/assets/images/#{video}.mp4"), filename: "#{video}.mp4", content_type: 'video/mp4')
-  file = URI.open('https://res.cloudinary.com/dcug1pvpk/video/upload/v1716529190/lazyreel-seeds/story-video-1.mp4')
+  file = URI.open(video)
   source.video.attach(io: file, filename: "#{video}.mp4", content_type: 'video/mp4')
   source.url = source.video.url
   source.save!
-  puts "Seeded Source ##{index + 1} with video #{video}, url: #{source.url}"
+  puts "(sources) Seeded Source ##{index + 1} with video #{video}, url: #{source.url}"
 end
 
 puts "Created #{videos.size} and attached videos"
