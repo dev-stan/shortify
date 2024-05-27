387dfa91f4e8354842ac13bd60e19576f5b8a81b
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-27 08:07:18 +0200

    updated reddit api

--- app/services/reddit_post.rb
+++ app/services/reddit_post.rb
@@ -1,39 +1,39 @@
-# /mnt/data/get_top_askreddit_post.rb
-
 require 'net/http'
 require 'uri'
 require 'json'
+require 'dotenv/load'
 
 class RedditPost
 
   def initialize(subreddit)
-    @subreddit = 'stories'
+    @subreddit = subreddit.to_s
   end
-
 
   CLIENT_ID = ENV['REDDIT_CLIENT_ID']
   SECRET = ENV['REDDIT_SECRET']
   USER_AGENT = 'RubyScript/1.0'
 
   AUTH_URL = 'https://www.reddit.com/api/v1/access_token'
-  TOP_POST_URL = "https://oauth.reddit.com/r/stories/top?limit=1&t=day"
-
 
   def top_post
     token = get_token
-    top_post = fetch_top_post(token)
+    top_posts = fetch_top_posts(token, limit: 10)
 
-    content = top_post['selftext']
-    title = top_post['title']
-    url = top_post['url']
-    upvotes = top_post['score']
+    random_post = top_posts.sample
+    content = random_post['selftext']
+    title = random_post['title']
+    url = random_post['url']
+    upvotes = random_post['score']
+
+    if content.nil? || content.empty?
+      return title
+    end
 
     return content
   end
 
   private
 
-  # Get the OAuth token using client credentials
   def get_token
     uri = URI(AUTH_URL)
     req = Net::HTTP::Post.new(uri)
@@ -48,9 +48,9 @@ class RedditPost
     JSON.parse(res.body)['access_token']
   end
 
-  # Fetch the top post
-  def fetch_top_post(token)
-    uri = URI(TOP_POST_URL)
+  def fetch_top_posts(token, limit: 1)
+    top_posts_url = "https://oauth.reddit.com/r/#{@subreddit}/top?limit=#{limit}&t=month"
+    uri = URI(top_posts_url)
     req = Net::HTTP::Get.new(uri)
     req['Authorization'] = "Bearer #{token}"
     req['User-Agent'] = USER_AGENT
@@ -59,7 +59,6 @@ class RedditPost
       http.request(req)
     end
 
-    JSON.parse(res.body)['data']['children'].first['data']
-    # JSON.parse(res.body)['data']['children'].first['data']
+    JSON.parse(res.body)['data']['children'].map { |post| post['data'] }
   end
 end
