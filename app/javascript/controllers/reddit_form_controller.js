e77ec94fff7c37d2d4ac0677d6c4182ee80b3bf2
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-27 11:37:46 +0200

    lots of api changes and added title

--- app/javascript/controllers/reddit_form_controller.js
+++ app/javascript/controllers/reddit_form_controller.js
@@ -1,23 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form", "fsubmit"];
+  static targets = ["title", "content", "fsubmit"];
 
   connect() {
     console.log("Controller connected");
-    console.log(this.formTarget);
+    console.log(this.contentTarget);
   }
 
   fetch_backend(event) {
-    event.preventDefault(); // Prevent the default form submission action
-    const query = this.fsubmitTarget.value;
+    event.preventDefault(); // Prevent the default content submission action
+    const query = event.currentTarget.value;
     const url = `/sources/reddit?query=${query}`;
     console.log(url);
     fetch(url, { headers: { Accept: "application/json" } })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-          this.formTarget.value = data.string
+          this.titleTarget.value = data.title
+          this.contentTarget.value = data.content
       });
   }
 }
