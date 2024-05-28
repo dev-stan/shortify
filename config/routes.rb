73159de528b2c28abba67828c7edccdd89469be3
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-28 12:36:42 +0200

    change settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/sources/reddit', to: 'sources#reddit'
+  get '/sources/video', to: 'sources#video'
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
