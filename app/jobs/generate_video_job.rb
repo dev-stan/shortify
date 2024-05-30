9680f3cab648eab8add6d2bc8464fe3a868ee4ec
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-30 13:53:45 +0200

    sidekiq changes

--- app/jobs/generate_video_job.rb
+++ app/jobs/generate_video_job.rb
@@ -1,7 +1,9 @@
 class GenerateVideoJob < ApplicationJob
   queue_as :default
 
-  def perform(source_url, script)
-    GenerateVideo.new(source_url, script).final_video_link
+  def perform(source_url, script, output)
+    new_url = GenerateVideo.new(source_url, script).final_video_link
+    output.url = new_url
+    output.save!
   end
 end
