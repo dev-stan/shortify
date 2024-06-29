a528b8ee8d385ac9308efb0f70216394aecd9b2b
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-06-29 18:29:12 +0200

    uncomment

--- app/services/reddit_post.rb
+++ app/services/reddit_post.rb
@@ -1,83 +1,79 @@
-# require 'net/http'
-# require 'uri'
-# require 'json'
-# require 'dotenv/load'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'dotenv/load'
 
-# class RedditPost
-#   def initialize(subreddit)
-#     @subreddit = subreddit.to_s
-#   end
 
-#   CLIENT_ID = ENV['REDDIT_CLIENT_ID']
-#   SECRET = ENV['REDDIT_SECRET']
-#   USER_AGENT = 'RubyScript/1.0'
+class RedditPost
+  def initialize(subreddit)
+    @subreddit = subreddit.to_s
+  end
 
-#   AUTH_URL = 'https://www.reddit.com/api/v1/access_token'
+  CLIENT_ID = ENV['REDDIT_CLIENT_ID']
+  SECRET = ENV['REDDIT_SECRET']
+  USER_AGENT = 'RubyScript/1.0'
 
-#   def top_posts
-#     token = get_token
-#     limit = 200
-#     after = nil
-#     suitable_posts = []
+  AUTH_URL = 'https://www.reddit.com/api/v1/access_token'
 
-#     n = 0
-#     while suitable_posts.size < 9
-#       n += 1
-#       break if n > 15
+  def top_posts
+    token = get_token
+    limit = 200
+    after = nil
+    suitable_posts = []
 
-#       top_posts, after = fetch_top_posts(token, limit: limit, after: after)
+    n = 0
+    while suitable_posts.size < 9
+      n += 1
+      break if n > 15
 
-#       top_posts.each do |post|
-#         content = post['selftext']
-#         title = post['title']
-#         id = post['id']
+      top_posts, after = fetch_top_posts(token, limit: limit, after: after)
 
-#         next if content.nil? || content.empty?
+      top_posts.each do |post|
+        content = post['selftext']
+        title = post['title']
+        id = post['id']
 
-#         if content.split.size <= 250 && suitable_posts.none? { |p| p[:id] == id }
-#           suitable_posts << { content: content, title: title, id: id }
-#           break if suitable_posts.size == 9
-#         end
-#       end
-#     end
+        next if content.nil? || content.empty?
 
-#     return suitable_posts.size == 9 ? suitable_posts : [{ content: 'no', title: 'no', id: 0 }]
-#   end
+        if content.split.size <= 250 && suitable_posts.none? { |p| p[:id] == id }
+          suitable_posts << { content: content, title: title, id: id }
+          break if suitable_posts.size == 9
+        end
+      end
+    end
 
-#   private
+    return suitable_posts.size == 9 ? suitable_posts : [{ content: 'no', title: 'no', id: 0 }]
+  end
 
-#   def get_token
-#     uri = URI(AUTH_URL)
-#     req = Net::HTTP::Post.new(uri)
-#     req.basic_auth(CLIENT_ID, SECRET)
-#     req.set_form_data('grant_type' => 'client_credentials')
-#     req['User-Agent'] = USER_AGENT
+  private
 
-#     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-#       http.request(req)
-#     end
+  def get_token
+    uri = URI(AUTH_URL)
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth(CLIENT_ID, SECRET)
+    req.set_form_data('grant_type' => 'client_credentials')
+    req['User-Agent'] = USER_AGENT
 
-#     JSON.parse(res.body)['access_token']
-#   end
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
 
-#   def fetch_top_posts(token, limit: 1, after: nil)
-#     top_posts_url = "https://oauth.reddit.com/r/#{@subreddit}/top?limit=#{limit}&t=year"
-#     top_posts_url += "&after=#{after}" if after
-#     uri = URI(top_posts_url)
-#     req = Net::HTTP::Get.new(uri)
-#     req['Authorization'] = "Bearer #{token}"
-#     req['User-Agent'] = USER_AGENT
+    JSON.parse(res.body)['access_token']
+  end
 
-#     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-#       http.request(req)
-#     end
+  def fetch_top_posts(token, limit: 1, after: nil)
+    top_posts_url = "https://oauth.reddit.com/r/#{@subreddit}/top?limit=#{limit}&t=year"
+    top_posts_url += "&after=#{after}" if after
+    uri = URI(top_posts_url)
+    req = Net::HTTP::Get.new(uri)
+    req['Authorization'] = "Bearer #{token}"
+    req['User-Agent'] = USER_AGENT
 
-#     data = JSON.parse(res.body)['data']
-#     [data['children'].map { |post| post['data'] }, data['after']]
-#   end
-# end
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
 
-# # Usage example:
-# # reddit = RedditPost.new('subreddit_name')
-# # posts = reddit.top_posts
-# # puts posts
+    data = JSON.parse(res.body)['data']
+    [data['children'].map { |post| post['data'] }, data['after']]
+  end
+end
